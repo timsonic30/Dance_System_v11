7ad9745a5ee535192e87e9698da120c029634927
timsonic30 <tim.lau@genstudents.org>
2025-03-24 10:28:51 +0800

    Add files via upload

---
+++ client/app/page.js
@@ -0,0 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
+import Main from "./components/Main";
+import Main2 from "./components/Main_v2";
+import Main3 from "./components/Main_v3";
+import Calendar2 from "./components/Calendar2";
+export default function Home() {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    try {
+      const response = await fetch("http://localhost:3030/");
+      const res = await response.json();
+      console.log(res);
+
+      if (res.message) {
+        setData(res.message);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log(err, err.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {/* <Calendar2 /> */}
+      <Main3 />
+      <div>This is the home page</div>
+      <div>{loading ? "I am loading" : data}</div>
+    </div>
+  );
+}
